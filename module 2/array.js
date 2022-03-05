15859fc2975a89ae6061425a0afd4c63f808aec3
preetirajput <rajput.preeti.singh@gmail.com>
2022-03-05 23:31:36 +0530

    push method

--- module 2/array.js
+++ module 2/array.js
@@ -9,4 +9,10 @@ console.log("Element at the index 5 : "+arr1[5]);
 arr1[2] = "string";
 console.log(arr1);
 
+console.log("array method");
 
+console.log("push method");
+let arr2 = [2,8,5,"value",9,"end"];
+arr2.push ("push");
+arr2.push (3);
+console.log(arr2);
