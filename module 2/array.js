9f7b3cb52e4e8b2771a03972c8a600b09c22ae32
preetirajput <rajput.preeti.singh@gmail.com>
2022-03-05 23:20:04 +0530

    print  value of a index

--- module 2/array.js
+++ module 2/array.js
@@ -3,3 +3,10 @@ console.log(arr);
 
 let arr1 = [1,2,3,4,"string1","string2",5,6];
 console.log(arr1);
+
+console.log("Element at the index 5 : "+arr1[5]);
+ 
+arr1[2] = "string";
+console.log(arr1);
+
+
