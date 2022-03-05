11f0ac4b1ea315b4da41e93995d2a5dbbb9d2a89
preetirajput <rajput.preeti.singh@gmail.com>
2022-03-05 22:57:06 +0530

    IIFE

--- module 2/function.js
+++ module 2/function.js
@@ -2,3 +2,30 @@ function sayHallo(){
     console.log("Hallo to function");
 }
 sayHallo();
+
+function sum(num1,num2){
+   let add = num1 + num2;
+   console.log(add);
+}
+sum(3,5);
+
+function multipiy(num1,num2){
+    return (num1*num2);
+}
+let ans = multipiy(3,5);
+console.log(ans);
+
+let a = function sub(num1,num2){
+    return num1-num2;
+}
+console.log(a(5,3));
+
+//IIFE Immediately InvokedFunction Expression
+(function(){
+    console.log("hello from IIFE");
+})();
+
+(function(num1,num2){
+    let num=num1/num2;
+    console.log(num);
+})(15,3);
